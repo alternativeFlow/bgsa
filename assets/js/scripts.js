1146fbf95c91bea478c04684a7521726ab16d942
alternativeFlow <danxue@gmail.com>
2020-06-08 13:26:39 -0700

    backup

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,13 +5,16 @@ window.onscroll = function() {makeNavSticky()};
 function makeNavSticky() {
 
 	var navbar = document.getElementById("sideNav");
+	var mainContent = document.getElementById("contentContainer");
 
 	var stickyOffset = navbar.offsetTop;
 
 	if (window.pageYOffset >= 260) {
-		navbar.classList.add("sticky"); 
+		navbar.classList.add("sticky");
+		mainContent.classList.add("whenSticky"); 
 	} 
 	else {
 		navbar.classList.remove("sticky");
+		mainContent.classList.remove("whenSticky"); 
 	}
 };
