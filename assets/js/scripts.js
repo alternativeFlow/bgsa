07e8337a425b337e475255597d9e68cd5376d743
alternativeFlow <danxue@gmail.com>
2020-09-16 15:12:32 -0700

    in progress save

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,20 +1,25 @@
 //STICKY NAVBAR
-window.onscroll = function() {makeNavSticky()};
 
+window.onscroll = function() {makeStickyNav()};
 
-function makeNavSticky() {
+function makeStickyNav() {
+    var sideNav = document.getElementById("sideNav");
+    var horizontalNav = document.getElementById("horizontalNavbar");
+    var mainContent = document.getElementById("mainContent");
 
-	var navbar = document.getElementById("sideNav");
-	var mainContent = document.getElementById("contentContainer");
+    var yOffset = sideNav.offsetTop - 40;
 
-	var stickyOffset = navbar.offsetTop;
+    if (window.pageYOffset >= yOffset) {
+        console.log(sideNav.offsetTop);
+        sideNav.classList.add("position-fixed");
+        sideNav.classList.add("sticky");
+        mainContent.classList.add("col-md-offset-3");
+    }
+    if (window.pageYOffset <= (horizontalNav.offsetTop+50))
+ {
+        sideNav.classList.remove("position-fixed");
+        sideNav.classList.remove("sticky");
+        mainContent.classList.remove("col-md-offset-3");
+    }
 
-	if (window.pageYOffset >= 260) {
-		navbar.classList.add("sticky");
-		mainContent.classList.add("whenSticky"); 
-	} 
-	else {
-		navbar.classList.remove("sticky");
-		mainContent.classList.remove("whenSticky"); 
-	}
-};
+}
